8d07204219c89952ad8ecfff9fece4ecefb48409
emilykpritchard <emilykpritchard@me.com>
2022-12-09 12:19:28 +0000

    added cors

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 import express from "express";
 import  christmasRouter from "./routes/christmasList.js";
-//import cors from "cors";
+import cors from "cors";
 
 const app = express();
-//app.use(cors())
+app.use(cors())
 
 app.use(express.json());
 app.use("/api/christmasList", christmasRouter);
